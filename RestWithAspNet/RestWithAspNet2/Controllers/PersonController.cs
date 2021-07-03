b18434b98e3213c4a8d964e3eeb21ce223a0a3e2
Jorel Magatti <jorel_dev@hotmail.com>
2021-07-03 13:29:38 -0300

    novo projeto api rest para estudo de chamadas get, post, put, delete

---
+++ RestWithAspNet/RestWithAspNet2/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        #region Private Parameters
+        private readonly ILogger<PersonController> _logger;
+        #endregion
+
+        #region Constructor
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+        #endregion
+
+        #region Get Call Controller Actions
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+        #endregion
+    }
+}
---
+++ RestWithAspNet/RestWithAspNet2/Model/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet2.Model
+{
+    public class PersonModel
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+    }
+}
