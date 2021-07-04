741138d33048dbeb7f843fe7ec24b7dc295ca36d
Jorel Magatti <jorel_dev@hotmail.com>
2021-07-04 13:55:15 -0300

    Adicionado ADO para MySql com entity

---
+++ RestWithAspNet/DAO_MySql/ADO/MySqlContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Pomelo.EntityFrameworkCore.MySql;
+using Entidades;
+
+namespace DAO_MySql.ADO
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext()
+        {
+
+        }
+
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+        
+        public DbSet<TB_PERSON> TB_PERSON { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = getConnection();
+            optionsBuilder.UseMySql(connectionString);
+        }
+
+        public static string getConnection()
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings["cslog"].ConnectionString;
+        }
+
+
+    }
+}
